2d9417b2d40448f86d1df7fcd52d2134a54c25ea
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-05-10 13:31:55 +0200

    Fix certificates listing

--- model/secret.go
+++ model/secret.go
@@ -21,8 +21,8 @@ func (s *Secret) IsCertificate() bool {
 func SecretParseList(rows []string) ([]Secret, error) {
 	valueRE := regexp.MustCompile("[^\\s]+\\s+([^\\s]+)")
 	result := []Secret{}
+	rows = rows[1:]
 	for len(rows) > 2 {
-
 		name := valueRE.FindStringSubmatch(rows[0])[1]
 		createdAt := valueRE.FindStringSubmatch(rows[1])[1]
 		updatedAt := valueRE.FindStringSubmatch(rows[2])[1]
